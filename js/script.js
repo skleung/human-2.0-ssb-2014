27ba782eb3e912c6d59cb8543b3189db6c3450c9
Sherman Leung <skleung@stanford.edu>
2014-02-15 16:46:12 -0800

    Finished skeleton

--- js/script.js
+++ js/script.js
@@ -1,47 +1,11 @@
 $(document).ready(function($) { 
-	$('.welcome-wrapper') .css({'min-height': (($(window).height()) - $('.enter').height() ) + 'px'}); //allows space for the enter section
-	$('#about').css({'min-height': $(window).height()+ 'px'}); //allows space for the footer section
-	// $('#projects').css({'min-height': (($(window).height()) - $('.footer').height() ) + 'px'}); //allows space for the footer section
-	$('.welcome-text').css({
-		left: ($(window).outerWidth() - $('.welcome-text').outerWidth()) / 2 + 'px',
-		top: ($(window).outerHeight() - $('.welcome-text').outerHeight()) / 2 + 'px'
-	});
+	$('.landing-photo') .css({'min-height': (($(window).height()) + 'px'}); //allows space for the enter section
 });
 $(window).resize(function() {
-	$('.welcome-wrapper') .css({'min-height': (($(window).height()) - $('.enter').height() ) + 'px'}); //allows space for the enter section
-	$('#about').css({'min-height': $(window).height()+ 'px'}); //allows space for the footer section
-	$('.welcome-wrapper').css({
-		left: ($(window).outerWidth() - $('.welcome-text').outerWidth()) / 2 + 'px',
-		top: ($(window).outerHeight() - $('.welcome-text').outerHeight()) / 2 + 'px'
-	});
+	$('.landing-photo') .css({'min-height': (($(window).height())+ 'px'}); //allows space for the enter section
 });
 
 $(document).ready(function(){
-	//Toggle between projects and about section
-	$(".toggle").click(function(event){
-		var sectionArray = ["about", "projects", "research" , "publications", "contact", "class"];
-		for (var i=0; i<sectionArray.length; i++){
-			$("."+sectionArray[i]+"-text").hide();
-		}
-		var sectionId = this.id;
-		$("."+sectionId+"-text").fadeIn();
-	});
-	$(".toggleAbout").click(function(event){
-		$(".project-text").hide();
-		$(".about-text").fadeIn();
-	});
-	var curProject;
-	$(".project-box").click(function(event){
-		var projectId = this.id;
-		if (projectId === curProject){
-			curProject = ""; //resets curProject so it can slide down again
-			$('.project-specs#'+projectId).slideUp();
-		}else{
-			curProject = projectId;
-			$('.project-specs').hide();
-			$('.project-specs#'+projectId).slideDown();
-		}
-	});
 	//Scrolling animation
 	$(".scroll").click(function(event){
 		event.preventDefault();
